d5ff3e1e3f49adddad7423710c05f5b849b941e1
imen-nmn <imen.nmn@gmail.com>
2017-11-10 12:09:43 +0100

    Fix back stack problem https://stackoverflow.com/questions/47219023/fragment-does-not-pop-out-from-backstack

--- app/src/main/java/com/imennmn/backstackexample/MainActivity.java
+++ app/src/main/java/com/imennmn/backstackexample/MainActivity.java
@@ -2,9 +2,11 @@ package com.imennmn.backstackexample;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements OnFragmentInteractionListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,4 +27,29 @@ public class MainActivity extends AppCompatActivity implements OnFragmentInterac
             ex.printStackTrace();
         }
     }
+
+
+    @Override
+    public void onBackPressed() {
+        Fragment currentFragment= getSupportFragmentManager().findFragmentById(R.id.frame);
+        super.onBackPressed();
+
+        if(currentFragment!=null && currentFragment.isVisible()) {
+            getSupportFragmentManager().beginTransaction().remove(currentFragment).commit();
+            getSupportFragmentManager().popBackStack(currentFragment.getTag(), FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        }
+    }
+
+//    @Override
+//    public void onBackPressed() {
+//        if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
+//            Fragment fragment = getSupportFragmentManager().findFragmentByTag(mFragment.getClass().getName());
+//            if (fragment != null && fragment.getClass().getName().equalsIgnoreCase(CFragment.class.getName())) {
+//                getSupportFragmentManager().beginTransaction().remove(mFragment).commit();
+//            }
+//            getSupportFragmentManager().popBackStackImmediate();
+//        } else {
+//            super.onBackPressed();
+//        }
+//    }
 }
